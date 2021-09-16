c292f7e7eb67320c230ab7465028247419fc5a28
KrystynaGrzesiak <krygrz11@gmail.com>
2021-09-16 19:52:16 +0200

    adds example app #1

---
+++ spotkanie1/app_source.R
@@ -0,0 +1,101 @@
+library(dplyr)
+library(ggdendro)
+library(tidyr)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+
+create_dend <- function(x, col_name1, col_name2) {
+    pivot_wider(x, names_from = c(col_name2),
+                values_from = "value") %>%
+        as.data.frame() %>% {
+            rownames(.) <- .[[col_name1]]
+            .
+        } %>%
+        select(-1) %>%
+        as.matrix() %>%
+        dist() %>%
+        hclust() %>%
+        as.dendrogram()
+}
+
+get_legend <- function(gg_plot) {
+    grob_table <- ggplotGrob(gg_plot)
+    grob_table[["grobs"]][[which(sapply(grob_table[["grobs"]], function(x) x[["name"]]) == "guide-box")]]
+}
+
+arrange_dendrogram <- function(heatmap,
+                               dendrogram_top,
+                               dendrogram_right,
+                               top_right = NULL,
+                               widths = c(0.8, 0.2),
+                               heights = c(0.2, 0.8)) {
+
+    grob_list <- list(heatmap = ggplotGrob(heatmap),
+                      dendrogram_top = ggplotGrob(dendrogram_top),
+                      dendrogram_right = ggplotGrob(dendrogram_right))
+
+    if(is.null(top_right))
+        top_right <- grid.rect(gp = gpar(col = NA), draw = FALSE)
+
+    max_width <- unit.pmax(grob_list[["heatmap"]][["widths"]],
+                           grob_list[["dendrogram_top"]][["widths"]])
+
+    grob_list[["heatmap"]][["widths"]] <-
+        grob_list[["dendrogram_top"]][["widths"]] <-
+        max_width
+
+    max_height <- unit.pmax(grob_list[["heatmap"]][["heights"]],
+                            grob_list[["dendrogram_right"]][["heights"]])
+
+    grob_list[["heatmap"]][["heights"]] <-
+        grob_list[["dendrogram_right"]][["heights"]] <-
+        max_height
+
+    grid.arrange(grob_list[["dendrogram_top"]],
+                 top_right,
+                 grob_list[["heatmap"]],
+                 grob_list[["dendrogram_right"]],
+                 widths = widths, heights = heights)
+
+}
+
+plot_dendrogram <- function(dendro) {
+    dendro %>%
+        dendro_data %>%
+        segment %>%
+        ggplot(aes(x = x, y = y, xend = xend, yend = yend)) +
+        geom_segment() +
+        scale_y_continuous("") +
+        scale_x_discrete("",
+                         limits = factor(1L:nobs(dendro)),
+                         labels = labels(dendro))
+}
+
+
+generate_plot <- function(dat) {
+
+    nm <- colnames(dat)
+
+    dat_dend1 <- create_dend(dat, nm[1], nm[2])
+    dat_dend2 <- create_dend(dat, nm[2], nm[1])
+
+    dendro_top <- plot_dendrogram(dat_dend1)
+    dendro_right <- plot_dendrogram(dat_dend2) + coord_flip()
+
+    hm <- ggplot(dat, aes(dat[, 1], dat[, 2], z = dat[, 3], fill= as.factor(dat[, 3]))) +
+        geom_tile(color="black") +
+        scale_fill_discrete(name = nm[3]) +
+        xlab(nm[1]) +
+        ylab(nm[2])
+
+    plt <- arrange_dendrogram(hm + theme_bw() + theme(legend.position = "bottom"),
+                              dendro_top + theme_void(), dendro_right + theme_void())
+
+    return(plt)
+
+}
+
+
+
+
---
+++ spotkanie1/shiny-app.R
@@ -0,0 +1,56 @@
+
+library(shiny)
+
+source("app_source.R")
+
+
+ui <- fluidPage(
+
+    titlePanel("Dzień dobry!"),
+
+    sidebarLayout(
+
+        sidebarPanel(
+            fileInput("wprowadzone_dane", "Wybierz dane csv z komputera:",
+                      accept = c(
+                          "text/csv",
+                          "text/comma-separated-values,text/plain",
+                          ".csv")
+            ),
+            "Wybrałeś:",
+            verbatimTextOutput("table")
+        ),
+
+        mainPanel(
+            plotOutput("heatmap")
+        )
+    )
+
+)
+
+server <- function(input, output, session) {
+
+
+    data <- reactive({
+
+        inFile <- input[["wprowadzone_dane"]]
+
+        if (is.null(inFile)) return(NULL)
+
+        read.csv(inFile$datapath)[-1]
+
+    })
+
+    output[["heatmap"]] <- renderPlot({
+
+        if(is.null(data())) return(NULL)
+
+        generate_plot(data())
+
+    })
+
+    output[["table"]] <- renderPrint({ head(data()) })
+
+}
+
+shinyApp(ui, server)
