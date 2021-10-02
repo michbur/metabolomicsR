0800fd942250ffec1f66b12e0ba7b167b503a258
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2021-10-02 23:41:21 +0200

    updated presentation

--- spotkanie2/mzR.R
+++ spotkanie2/mzR.R
@@ -2,8 +2,10 @@ library(mzR)
 
 fl <- system.file("threonine", "threonine_i2_e35_pH_tree.mzXML",
                   package = "msdata")
-ms_fl <- openMSfile(fl, backend = "pwiz")
+ms_fl <- openMSfile(fl, backend = "pwiz", verbose = TRUE)
 ## Get the spectra
 pks <- spectra(ms_fl)
 ## Get the header
 hdr <- header(ms_fl)
+
+write.csv(hdr, file = "threonine-header.csv")
